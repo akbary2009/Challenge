1adf4aa266539d5afde7d023ef34b33b5ec18c07
user <user@localhost>
2025-09-05 17:09:11 +0430

    Euler: 25

--- Challenge/Program.cs
+++ Challenge/Program.cs
@@ -10,7 +10,8 @@ namespace Challenge
             //var sw = new System.Diagnostics.Stopwatch();
             //sw.Start();
             //var result = ProjectEuler.P22.GetAnswer();
-            var result = ProjectEuler.P23.GetAnswer();
+            //var result = ProjectEuler.P23.GetAnswer();
+            var result = ProjectEuler.P25.GetAnswer();
 
             //sw.Stop();
             //Console.WriteLine(sw.Elapsed.ToString());
---
+++ Challenge/ProjectEuler/P25.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace Challenge.ProjectEuler
+{
+    public class P25
+    {
+        public static int GetAnswer()
+        {
+            BigInteger fib1 = 1, fib2 = 1, fib;
+
+            for (int i = 3; i <= int.MaxValue; i++)
+            {
+                fib = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = fib;
+
+                if (fib.ToString().Length == 1000)
+                    return i;
+            }
+
+            throw new Exception();
+        }
+    }
+}
