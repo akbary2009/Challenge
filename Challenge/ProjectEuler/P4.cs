1d684803dda44993f3533b9e714ad245aa0135c6
user <user@localhost>
2025-09-08 17:01:28 +0430

    Euler: 4

---
+++ Challenge/ProjectEuler/P4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Challenge.ProjectEuler
+{
+    /// <summary>
+    /// https://projecteuler.net/problem=4
+    /// </summary>
+    public static class P4
+    {
+        public static int GetAnswer()
+        {
+            return GetPalindromics()
+                .Max();
+        }
+
+        private static IEnumerable<int> GetPalindromics()
+        {
+            for (int i = 999; i > 99; i--)
+                for (int j = 999; j > 99; j--)
+                    if (IsPalindromic(i * j))
+                        yield return i * j;
+        }
+
+        private static bool IsPalindromic(int number)
+            => number.ToString().Equals(number.ToString()
+                .Reverse()
+                .Aggregate(string.Empty, (a, b) => a + b));
+    }
+}
