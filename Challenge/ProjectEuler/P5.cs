2c1ced736101c177eba523f368ee63d20e9a6288
user <user@localhost>
2025-09-10 17:09:40 +0430

    Euler: 5

---
+++ Challenge/ProjectEuler/P5.cs
@@ -0,0 +1,24 @@
+﻿namespace Challenge.ProjectEuler
+{
+    /// <summary>
+    /// https://projecteuler.net/problem=5
+    /// </summary>
+    public static class P5
+    {
+        public static int GetAnswer()
+        {
+            for (int i = 20; i < int.MaxValue; i++)
+                if (IsDivisible(i)) return i;
+
+            return 0;
+        }
+
+        private static bool IsDivisible(int number)
+        {
+            for (int i = 2; i <= 20; i++)
+                if (number % i != 0) return false;
+
+            return true;
+        }
+    }
+}
