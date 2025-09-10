7784cf450c50119651bcd2cb5b7bf17afd87ffee
user <user@localhost>
2025-09-10 17:18:00 +0430

    Euler: 6

---
+++ Challenge/ProjectEuler/P6.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Challenge.ProjectEuler
+{
+    /// <summary>
+    /// https://projecteuler.net/problem=6
+    /// </summary>
+    public static class P6
+    {
+        public static int GetAnswer()
+        {
+            var nums = Enumerable.Range(1, 100);
+            var squareSum = nums
+                .Select(n => Math.Pow(n, 2))
+                .Sum();
+
+            return (int)(Math.Pow(nums.Sum(), 2) - squareSum);
+        }
+    }
+}
