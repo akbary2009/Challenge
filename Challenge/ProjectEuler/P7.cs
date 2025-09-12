80bd3edc4a7ec6a063c52ea07703f7fedbd1ad1c
user <user@localhost>
2025-09-12 16:16:50 +0430

    Euler: 7

---
+++ Challenge/ProjectEuler/P7.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Challenge.ProjectEuler
+{
+    /// <summary>
+    /// https://projecteuler.net/problem=7
+    /// </summary>
+    public static class P7
+    {
+        public static int GetAnswer()
+        {
+            return GetPrimeNumbers()
+                .Skip(10_001 - 1)
+                .Take(1)
+                .FirstOrDefault();
+        }
+
+        private static IEnumerable<int> GetPrimeNumbers()
+        {
+            yield return 2;
+
+            for (int i = 3; i < int.MaxValue; i += 2)
+                if (IsPrime(i))
+                    yield return i;
+        }
+
+        private static bool IsPrime(int number)
+        {
+            for (int i = 2; i < number / 2; i++)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
