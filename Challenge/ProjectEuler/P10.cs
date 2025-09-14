d5ad26e182dc1328459467d2361e2dd2a025647a
user <user@localhost>
2025-09-14 17:08:40 +0430

    Euler: 10

---
+++ Challenge/ProjectEuler/P10.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Challenge.ProjectEuler
+{
+    /// <summary>
+    /// https://projecteuler.net/problem=10
+    /// </summary>
+    public static class P10
+    {
+        public static long GetAnswer()
+        {
+            return GetPrimes(2_000_000).Sum();
+        }
+
+        private static IEnumerable<long> GetPrimes(int maxValue)
+        {
+            for (int i = 2; i < maxValue; i++)
+                if (IsPrime(i))
+                    yield return i;
+        }
+
+        private static bool IsPrime(int number)
+        {
+            for (int i = 2; i <= number / 2; i++)
+                if (number % i == 0) return false;
+
+            return true;
+        }
+    }
+}
