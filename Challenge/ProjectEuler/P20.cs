0d3e07898b335869ef7cf50b9059bde4c5bbe2f1
user <user@localhost>
2025-09-20 17:03:48 +0430

    Euler: 20

---
+++ Challenge/ProjectEuler/P20.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Numerics;
+
+namespace Challenge.ProjectEuler
+{
+    /// <summary>
+    /// https://projecteuler.net/problem=20
+    /// </summary>
+    public static class P20
+    {
+        public static int GetAnswer()
+        {
+            return Fact(100)
+                .ToString()
+                .Select(c => c.ToString())
+                .Select(int.Parse)
+                .Aggregate(0, (a, b) => a + b);
+        }
+
+        private static BigInteger Fact(long number)
+        {
+            if (number == 1) return 1;
+
+            return number * Fact(number - 1);
+        }
+    }
+}
