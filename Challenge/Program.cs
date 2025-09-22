6e4a613dd6e40f123534f5a222b15cc32dd0a412
user <user@localhost>
2025-09-22 15:35:21 +0330

    Euler: 29

--- Challenge/Program.cs
+++ Challenge/Program.cs
@@ -10,7 +10,7 @@ namespace Challenge
             //var sw = new System.Diagnostics.Stopwatch();
             //sw.Start();
             //var result = ProjectEuler.P1.GetAnswer();
-            var result = ProjectEuler.P25.GetAnswer();
+            var result = ProjectEuler.P29.GetAnswer();
 
             //sw.Stop();
             //Console.WriteLine(sw.Elapsed.ToString());
---
+++ Challenge/ProjectEuler/P29.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Challenge.ProjectEuler
+{
+    /// <summary>
+    /// https://projecteuler.net/problem=29
+    /// </summary>
+    public static class P29
+    {
+        public static int GetAnswer()
+        {
+            return GetList()
+                .Distinct()
+                .Count();
+
+            IEnumerable<string> GetList()
+            {
+                for (int i = 2; i <= 100; i++)
+                {
+                    for (int j = 2; j <= 100; j++)
+                    {
+                        yield return Math.Pow(i, j).ToString();
+                    }
+                }
+            }
+        }
+    }
+}
